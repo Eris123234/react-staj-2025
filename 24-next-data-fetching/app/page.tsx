4c5382a42fceea5a7a4824ee572dd24cf7003304
Zafer AYAN <ozcanzaferayan@gmail.com>
2025-09-10 23:37:06 +0300

    add

---
+++ 24-next-data-fetching/app/page.tsx
@@ -0,0 +1,34 @@
+import ProductCard from "./_components/ProductCard";
+
+export type Product = {
+  id: number;
+  title: string;
+  price: number;
+  description: string;
+  category: string;
+  image: string;
+  rating: {
+    rate: number;
+    count: number;
+  };
+};
+
+const getProducts = async (): Promise<Product[]> => {
+  const API_URL = "https://fakestoreapi.com/products";
+  const res = await fetch(API_URL);
+  const products = await res.json();
+  return products;
+};
+
+const page = async () => {
+  const products = await getProducts();
+  return (
+    <ul className="grid max-w-7xl md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 p-4 mx-auto">
+      {products.map((product) => (
+        <ProductCard key={product.id} product={product} />
+      ))}
+    </ul>
+  );
+};
+
+export default page;
---
+++ 24-next-data-fetching/next.config.ts
@@ -0,0 +1,9 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  output: 'standalone',       // Next 13+ için
+  poweredByHeader: false,
+};
+
+export default nextConfig;
