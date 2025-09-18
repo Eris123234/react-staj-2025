7d64026c324e78cf9b5a640e2d64b6580ba09aac
Zafer AYAN <ozcanzaferayan@gmail.com>
2025-09-18 22:47:43 +0300

    Create mainscreen

--- 27-card-game/app/page.tsx
+++ 27-card-game/app/page.tsx
@@ -1,103 +1,108 @@
-import Image from "next/image";
+import {
+  ChevronDown,
+  Heart,
+  LayoutGrid,
+  Plus,
+  Search,
+  User,
+} from "lucide-react";
+import Link from "next/link";
 
-export default function Home() {
+const page = () => {
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <div
+      className="flex flex-col min-h-screen bg-gradient-to-b  
+    from-[#1F4EC5] via-[#CFC5F9] to-[#FBEFDF]"
+    >
+      <div
+        className="flex-1
+      p-8 pt-12 text-white flex flex-col gap-6
+    "
+      >
+        {/* Header */}
+        <div>
+          <span className="text-xl">Trending</span>
+          <div className="flex items-center gap-1">
+            <span className="text-3xl font-semibold">Questions</span>
+            <ChevronDown className="mt-1.5" />
+          </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+
+        {/* Info and Card container */}
+        <div className="flex flex-col flex-1 gap-4 relative">
+          {/* Swipe info Container */}
+          <div className="text-sm bg-black/35 rounded-full py-2 px-4 text-center">
+            Swipe left/right to check more
+          </div>
+
+          {/* Card */}
+          <div className="flex flex-1  bg-white rounded-3xl border-1 border-slate-400/20 shadow-2xl shadow-black/10">
+            <div className="flex flex-col justify-between gap-4 px-8 py-12 ">
+              <div className="">
+                {/* Tags */}
+                <div className="flex gap-2">
+                  <span className="font-base text-blue-500">#ui</span>
+                  <span className="font-base text-blue-500">#ux</span>
+                  <span className="font-base text-blue-500">#interaction</span>
+                </div>
+
+                {/* Question */}
+                <span className="text-slate-700 font-semibold text-2xl ">
+                  Why do people clear the screen multiple times when using a
+                  calculator?
+                </span>
+
+                {/* Answers & Viewed */}
+                <div className="flex gap-8 text-slate-500">
+                  <span>10 answers</span>
+                  <span>12k views</span>
+                </div>
+              </div>
+
+              {/* Question Creator */}
+              <div className="flex gap-4">
+                <img
+                  className="rounded-full w-12 h-12 bg-blue-500"
+                  src="https://api.dicebear.com/9.x/notionists/svg?seed=George"
+                  alt="avatar"
+                />
+                <div className="grid">
+                  <span className="text-slate-900 text-base font-semibold">
+                    Donat Twerski
+                  </span>
+                  <span className="text-slate-500 text-sm ">
+                    UX designer @uigate
+                  </span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      {/* Bottom navbar */}
+      <div className="bg-white pb-4 px-12 flex justify-between text-slate-500">
+        <Link href={"#"} className="border-t-4 pt-4 px-2 border-orange-500">
+          <LayoutGrid />
+        </Link>
+        <Link href={"#"} className="border-t-4 pt-4 px-2 border-orange-500/0">
+          <Search />
+        </Link>
+        <Link
+          href={"#"}
+          className="mt-4  bg-orange-500 rounded-full text-white p-1.5"
         >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+          <Plus />
+        </Link>
+        <Link href={"#"} className="border-t-4 pt-4 px-2 border-orange-500/0">
+          <Heart />
+        </Link>
+        <Link href={"#"} className="border-t-4 pt-4 px-2 border-orange-500/0">
+          <User />
+        </Link>
+      </div>
     </div>
   );
-}
+};
+
+export default page;
